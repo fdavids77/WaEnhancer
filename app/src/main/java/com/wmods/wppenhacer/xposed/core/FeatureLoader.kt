9a63dd48bc3de84e2f869e8b79f84751c6fcc2d4
Dev4Mod <wellingtonmods@gmail.com>
2026-07-11 14:29:40 -0300

    feat(FeatureLoader): add alert message when module package is not found

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/FeatureLoader.kt
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/FeatureLoader.kt
@@ -170,7 +170,6 @@ class FeatureLoader {
                 object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
                         mApp = param.args[0] as Application
-                        initializeModuleContext()
                         val application = mApp!!
                         val pref = getPreferences(application)
                         Feature.DEBUG = pref.getBoolean("enablelogs", true)
@@ -198,6 +197,7 @@ class FeatureLoader {
                         registerReceivers()
 
                         try {
+                            initializeModuleContext()
                             val timeMillis = System.currentTimeMillis()
                             UnobfuscatorCache.init(application)
                             SharedPreferencesWrapper.hookInit(application.classLoader)
@@ -315,8 +315,8 @@ class FeatureLoader {
                     Context.CONTEXT_INCLUDE_CODE or Context.CONTEXT_IGNORE_SECURITY
                 )
                 moduleContext = android.view.ContextThemeWrapper(context, R.style.AppTheme)
-            } catch (e: PackageManager.NameNotFoundException) {
-                throw PackageManager.NameNotFoundException("Module package not found")
+            } catch (_: PackageManager.NameNotFoundException) {
+                throw PackageManager.NameNotFoundException(Utils.application.getString(R.string.alert_module_notfound))
             }
         }
 
